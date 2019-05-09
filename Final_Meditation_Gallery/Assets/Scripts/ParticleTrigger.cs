ba9e481a7baf57c06da5018179dda0d8f5caaeda
Allen Li <lihaoyang411>
2019-05-09 13:11:10 -0600

    polishing

---
+++ Final_Meditation_Gallery/Assets/Scripts/ParticleTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private ParticleSystem fire;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            fire.Emit(1);
+        }
+    }
+
+}
